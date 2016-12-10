9ce42f8d0845ebc8910d27b7ee3e85207fdb7787
janko33 <janko33@example.com>
2016-12-10 16:36:12 +0100

    Thread 1 "bitcoind" received signal SIGSEGV, Segmentation fault. CBlockTreeDB::LoadBlockIndexGuts (this=<optimized out>) at txdb.cpp:359 pindexNew->pprev          = InsertBlockIndex(diskindex.hashPrev);

--- src/chain.h
+++ src/chain.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
+ // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -328,6 +328,7 @@ public:
 
     explicit CDiskBlockIndex(const CBlockIndex* pindex) : CBlockIndex(*pindex) {
         hashPrev = (pprev ? pprev->GetBlockHash() : uint256());
+        nHashBlock = *pindex->phashBlock;
     }
 
     ADD_SERIALIZE_METHODS;
